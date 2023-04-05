34aaf79d97f45d2664657886a715d1b1a5f613dd
Simon Farre <simon.farre.cx@gmail.com>
2023-04-05 22:50:58 +0200

    added release builds version as well

--- setup-local-sdk.sh
+++ setup-local-sdk.sh
@@ -7,8 +7,9 @@ usage() {
   echo "Clones the matrix-rust-sdk repository & the kotlin components used for bindings" 1>&2;
   echo "Be sure to run this command from within the radiator directory as this store\nconfiguration files there" 1>&2;
   echo -e "Usage: \n\t$0 [-c] - clones the sdk" 1>&2; 
-  echo -e "       \t$0 [-r] - clones the radiator fork of the sdk" 1>&2; 
+  echo -e "       \t$0 [-f] - clones the radiator fork of the sdk" 1>&2; 
   echo -e "       \t$0 [-b] - builds the sdk and copies files into radiator directory" 1>&2; 
+  echo -e "       \t$0 [-r] - release-builds the sdk and copies files into radiator directory" 1>&2; 
   exit 1;
 }
 
@@ -36,13 +37,13 @@ build_sdk() {
     echo "you must run $0 -c first"
     usage
   fi
-  ./matrix-sdk/matrix-rust-components-kotlin/scripts/build.sh -p ./matrix-sdk/matrix-rust-sdk -o ./libraries/rustsdk/matrix-rust-sdk.aar;
+  ./matrix-sdk/matrix-rust-components-kotlin/scripts/build.sh $1 -p ./matrix-sdk/matrix-rust-sdk -o ./libraries/rustsdk/matrix-rust-sdk.aar;
   exit 0;
 }
 
-while getopts "rcb" o; do
+while getopts "fcbr" o; do
     case "${o}" in
-        r)
+        f)
             echo "Cloning radiator fork of rust sdk"
             clone_into ./matrix-sdk git@github.com:theIDinside/matrix-rust-sdk.git
             clone_into ./matrix-sdk git@github.com:theIDinside/matrix-rust-components-kotlin.git
@@ -53,6 +54,9 @@ while getopts "rcb" o; do
             clone_into ./matrix-sdk "$kotlin_components"
             exit
             ;;
+        r)
+            build_sdk "-r"
+            ;;
         b)
             build_sdk
             ;;
@@ -61,3 +65,5 @@ while getopts "rcb" o; do
             ;;
     esac
 done
+
+usage
