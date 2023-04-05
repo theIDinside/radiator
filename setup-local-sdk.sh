51e81ed9a048bbfb166c836a659f4406b2532298
Simon Farre <simon.farre.cx@gmail.com>
2023-04-05 22:20:11 +0200

    Created utility script for cloning & building
    Script can clone the rust-sdk & cousin (kotlin-components).
    
    Running script with `-c` clones repos into ./matrix-sdk
    Running script with `-b` builds the sdk and copies final
    .aar file into libraries/rustsdk
    
    Todo: improve script to also check that all build tools
    exist on system

---
+++ setup-local-sdk.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+sdk_url="https://github.com/matrix-org/matrix-rust-sdk.git"
+kotlin_components="https://github.com/matrix-org/matrix-rust-components-kotlin.git"
+
+usage() { 
+  echo "Clones the matrix-rust-sdk repository & the kotlin components used for bindings" 1>&2;
+  echo "Be sure to run this command from within the radiator directory as this store\nconfiguration files there" 1>&2;
+  echo -e "Usage: \n\t$0 [-c] - clones the sdk" 1>&2; 
+  echo -e "       \t$0 [-b] - builds the sdk and copies files into radiator directory" 1>&2; 
+  exit 1;
+}
+
+clone_into() {
+  [ -d $1 ]
+  res=$?
+  if [ $res == 1 ]; then
+    mkdir $1
+  fi
+  echo "git -C $1 clone $2"
+  git -C $1 clone $2
+}
+
+build_sdk() {
+  [ -d ./matrix-sdk/matrix-rust-components-kotlin/ ]
+  res=$?
+  if [ $res == 1 ]; then
+    echo "you must run $0 -c first"
+    usage
+  fi
+  [ -d ./matrix-sdk/matrix-rust-sdk/ ]
+  res=$?
+ 
+  if [ $res == 1 ]; then
+    echo "you must run $0 -c first"
+    usage
+  fi
+  ./matrix-sdk/matrix-rust-components-kotlin/scripts/build.sh -p ./matrix-sdk/matrix-rust-sdk -o ./libraries/rustsdk/matrix-rust-sdk.aar;
+  exit 0;
+}
+
+while getopts "cb" o; do
+    case "${o}" in
+        c)
+            echo "Cloning projects to ${s}"
+            clone_into ./matrix-sdk "$sdk_url"
+            clone_into ./matrix-sdk "$kotlin_components"
+            ;;
+        b)
+            build_sdk
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
