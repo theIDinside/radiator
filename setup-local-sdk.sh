9057fe5df4a0183cd6daa6e8310da7be291f02cb
Simon Farre <simon.farre.cx@gmail.com>
2023-04-14 13:49:53 +0200

    Setup script for easily moving project between computers

--- setup-local-sdk.sh
+++ setup-local-sdk.sh
@@ -23,7 +23,71 @@ clone_into() {
   git -C $1 clone $2
 }
 
+has_dependency() {
+  command -v $1 &2>/dev/null
+  res=$?
+  return $res
+}
+
+has_cargo_dep() {
+  cargo --list | grep $1
+  res=$?
+  return $res
+}
+
+ask() {
+  echo $1
+  echo "Type y for yes and n for no (case senstitive. input must either be n or y, exactly)"
+  while read ans; do
+    if [ $ans == 'y' ]; then
+      return 0
+    elif [ $ans == 'n' ]; then
+      echo "Exiting. Toolchain verification aborted"
+      exit 1
+    fi
+    echo "You must type either y or n"
+  done
+}
+
+# Verifies the toolchain requirements & asks the user permission to install them if the script
+# can't find them installed on the system.
+verify_toolchain() {
+  # Check if protobuf compiler is installed, ask to install if not
+  if ! [ "$(has_dependency protoc)" ]; then
+    ask "Protoc is not installed. Install it now?"
+    res=$(command -v apt-get)
+    if [ $res == 0 ]; then
+      sudo apt-get install protoc
+    else
+      sudo dnf install protoc
+    fi
+  fi
+
+  # Check if rust toolchain is installed (cargo & rust compiler) 
+  # ask user if we should install otherwise
+  if ! [ "$(has_dependency cargo)" ]; then
+    echo "Cargo is not installed. You need to first install rustup"
+    ask "Install rust toolchain now?"
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    installedOk=$?
+    if [ $installedOk != 0 ]; then
+      echo "Installing cargo toolchain failed?"
+      exit 1
+    fi
+  fi
+
+  # Check if cargo-ndk is installed (cargo installed) - otherwise ask 
+  # to install
+  if ! [ $(has_cargo_dep ndk) ]; then
+      ask "cargo-ndk not installed. Install now?"
+      cargo install cargo-ndk
+  fi
+  # Add all Android targets for cargo
+  rustup target add aarch64-linux-android armv7-linux-androideabi x86_64-linux-android i686-linux-android
+}
+
 build_sdk() {
+  verify_toolchain
   [ -d ./matrix-sdk/matrix-rust-components-kotlin/ ]
   res=$?
   if [ $res == 1 ]; then
