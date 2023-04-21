7ca5511fe3fdd283fd8aa927908c1edfbc91e383
Simon Farre <simon.farre.cx@gmail.com>
2023-04-21 22:52:43 +0200

    Added loading animation and Message composer component
    Signed-off-by: Simon Farre <simon.farre.cx@gmail.com>

---
+++ app/src/main/java/com/app/radiator/ui/components/LoadingAnimation.kt
@@ -0,0 +1,63 @@
+package com.app.radiator.ui.components
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+fun LoadingAnimation(
+    circleColor: Color = Color.Magenta,
+    animationDelay: Int = 1000,
+) {
+
+    // circle's scale state
+    val circleScale = remember { mutableStateOf(0f) }
+    // animation
+    val circleScaleAnimate = animateFloatAsState(
+        targetValue = circleScale.value,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = animationDelay
+            )
+        )
+    )
+    // This is called when the app is launched
+    LaunchedEffect(Unit) {
+        circleScale.value = 1f
+    }
+    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        // animating circle
+        Box(
+            modifier = Modifier
+                .size(size = 128.dp)
+                .scale(scale = circleScaleAnimate.value)
+                .border(
+                    width = 4.dp,
+                    color = circleColor.copy(alpha = 1 - circleScaleAnimate.value),
+                    shape = CircleShape
+                )
+
+        ) {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/app/radiator/ui/components/MessageComposer.kt
@@ -0,0 +1,51 @@
+package com.app.radiator.ui.components
+
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.app.radiator.matrix.timeline.UserTimelineMessage
+
+@Preview
+@Composable
+fun MessageComposer(
+    modifier: Modifier = Modifier,
+    userTimelineMessage: UserTimelineMessage = UserTimelineMessage(),
+    sendMessageOp: (String) -> Unit = { Log.w("MessageComposer", "Send message not implemented") },
+) {
+    val (messageInput, setMessage) = remember { mutableStateOf("") }
+
+    fun sendMessage() {
+        sendMessageOp(messageInput)
+    }
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(width = 2.dp, color = Color.Gray)
+            .background(color = Color.White)
+    ) {
+        TextField(
+            modifier = Modifier
+                .weight(1f),
+            value = messageInput,
+            onValueChange = setMessage,
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Send),
+            keyboardActions = KeyboardActions { sendMessage() },
+            placeholder = { Text("Write message...") }
+        )
+    }
+}
