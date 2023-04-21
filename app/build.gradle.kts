8498f2b8f6d58eeb14f6d6f615def0ab04e0f550
Simon Farre <simon.farre.cx@gmail.com>
2023-04-21 17:32:01 +0200

    Added coil dependency (sigh) - this will be removed in future commits

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,7 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = composeVersion()
     }
-    packagingOptions {
+    packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
@@ -72,6 +72,8 @@ dependencies {
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
     implementation("androidx.compose.material3:material3:1.1.0-beta02")
+
+    implementation("io.coil-kt:coil-compose:2.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
