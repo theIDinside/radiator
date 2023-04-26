d7b776215d4cb8f5e802c1e8f490572d871b8991
Simon Farre <simon.farre.cx@gmail.com>
2023-04-26 22:19:54 +0200

    Refactored reply item composable

--- app/src/main/java/com/app/radiator/ui/components/ReplyItem.kt
+++ app/src/main/java/com/app/radiator/ui/components/ReplyItem.kt
@@ -2,15 +2,14 @@ package com.app.radiator.ui.components
 
 import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -20,8 +19,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
@@ -31,16 +29,16 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.app.radiator.matrix.timeline.displayName
 
+val ReplyBoxColor = Color(248, 248, 253)
+val ReplyBoxBorderColor = Color(238, 238, 243)
+
 val InReplyToText = AnnotatedString(
   "in reply to: ", spanStyles = listOf(
     AnnotatedString.Range(
       SpanStyle(
-        color = Color.Blue,
-        fontWeight = FontWeight.Bold,
-        textDecoration = TextDecoration.Underline
+        color = Color.Blue, fontWeight = FontWeight.Bold, textDecoration = TextDecoration.Underline
       ), 0, 11
-    ),
-    AnnotatedString.Range(
+    ), AnnotatedString.Range(
       SpanStyle(
         color = Color.Blue,
         fontWeight = FontWeight.Bold,
@@ -49,139 +47,92 @@ val InReplyToText = AnnotatedString(
   )
 )
 
+@Composable
+fun ReplyBox(content: @Composable () -> Unit) {
+  Box(modifier = Modifier
+    .padding(start = 11.dp)
+    .fillMaxWidth()
+    .clickable {
+      Log.i("ReplyItemClicked", "ReplyItemClicked")
+    }
+    .background(
+      color = ReplyBoxColor, shape = RoundedCornerShape(4.dp)
+    )
+    .border(width = 2.dp, color = ReplyBoxBorderColor, RoundedCornerShape(5.dp))
+    .clip(RoundedCornerShape(3.dp))) {
+    content()
+  }
+}
+
+@Composable
+fun InReplyToCard(avatarData: AvatarData?, onClickUserRepliedTo: () -> Unit) {
+  Row(modifier = Modifier.padding(start = 5.dp, top = 3.dp)) {
+    Text(text = InReplyToText)
+    Card(
+      modifier = Modifier.clickable { onClickUserRepliedTo() },
+      shape = RoundedCornerShape(9.dp),
+      elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)
+    ) {
+      Row(
+        modifier = Modifier,
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+      ) {
+        Spacer(Modifier.width(2.dp))
+        if (avatarData != null) Avatar(avatarData = avatarData, size = 13.dp)
+        Spacer(Modifier.width(5.dp))
+        Text(text = avatarData?.name ?: "unavailable")
+        Spacer(Modifier.width(5.dp))
+      }
+    }
+  }
+
+}
+
 @Preview
 @Composable
 fun ReplyItem(
   textThatWasRepliedTo: AnnotatedString = AnnotatedString("Foo\nBar"),
   avatarData: AvatarData? = AvatarData(
-    id = preview.sender,
-    name = preview.senderProfile.displayName(),
-    url = null
+    id = preview.sender, name = preview.senderProfile.displayName(), url = null
   ),
-  onClickInReplyTo: () -> Unit = {},
   onClickUserRepliedTo: () -> Unit = {},
 ) {
-  Box(
-    modifier = Modifier
-      .fillMaxWidth()
-      .clickable {
-        Log.i("ReplyItemClicked", "ReplyItemClicked")
-      }
-      .background(color = Color.White)
-      .padding(bottom = 5.dp)
-  ) {
-    Row() {
-      Column(Modifier.height(IntrinsicSize.Max), horizontalAlignment = Alignment.Start) {
-        Column(
-          modifier = Modifier
-            .background(
-              color = Color.White,
-              shape = RoundedCornerShape(4.dp, 4.dp, 0.dp, 4.dp)
-            )
-            .padding(start = 15.dp)
-            .drawBehind {
-              val borderSize = 2.dp.toPx()
-              drawLine(
-                color = Color.Black,
-                start = Offset(-5f, 0f),
-                end = Offset(-5f, size.height),
-                strokeWidth = borderSize
-              )
-            }
-        ) {
-          Row(modifier = Modifier.padding(start = 5.dp, top = 3.dp)) {
-            Text(modifier = Modifier.clickable { onClickInReplyTo() }, text = InReplyToText)
-            Card(
-              modifier = Modifier.clickable { onClickUserRepliedTo() },
-              shape = RoundedCornerShape(9.dp),
-              elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)
-            ) {
-              Row(
-                modifier = Modifier,
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically
-              ) {
-                Spacer(Modifier.width(2.dp))
-                if (avatarData != null)
-                  Avatar(avatarData = avatarData, size = 13.dp)
-                Spacer(Modifier.width(5.dp))
-                Text(text = avatarData?.name ?: "unavailable")
-                Spacer(Modifier.width(5.dp))
-              }
-            }
-          }
-          Text(
-            modifier = Modifier
-              .fillMaxWidth()
-              .padding(start = 5.dp), text = textThatWasRepliedTo
-          )
-        }
-      }
+  ReplyBox()
+  {
+    Column(
+      modifier = Modifier.replyModifier()
+    ) {
+      InReplyToCard(avatarData=avatarData, onClickUserRepliedTo = onClickUserRepliedTo)
+      Text(
+        modifier = Modifier
+          .fillMaxWidth()
+          .padding(start = 5.dp), text = textThatWasRepliedTo
+      )
     }
   }
 }
+
+fun Modifier.replyModifier(): Modifier = this
+  .padding(start = 5.dp, top = 5.dp, bottom = 5.dp)
+
 
 @Composable
 fun ReplyItemMessageNode(
   rootMessageNode: ParsedMessageNode,
   avatarData: AvatarData?,
-  onClickInReplyTo: () -> Unit = {},
   onClickUserRepliedTo: () -> Unit = {},
 ) {
-  Box(
-    modifier = Modifier
-      .fillMaxWidth()
-      .background(color = Color.White)
-      .clickable {
-        Log.i("ReplyItemMessageNodeClicked", "ReplyItemMessageNodeClicked")
-      }
-  ) {
-    Row() {
-      Column(
-        modifier = Modifier
-          .background(
-            color = Color.White,
-            shape = RoundedCornerShape(4.dp, 4.dp, 0.dp, 4.dp)
-          )
-          .padding(start = 15.dp)
-          .drawBehind {
-            val borderSize = 2.dp.toPx()
-            drawLine(
-              color = Color.Black,
-              start = Offset(-5f, 0f),
-              end = Offset(-5f, size.height),
-              strokeWidth = borderSize
-            )
-          }
-      ) {
-        Row(modifier = Modifier.padding(start = 5.dp, top = 3.dp)) {
-          Text(modifier = Modifier.clickable { onClickInReplyTo() }, text = InReplyToText)
-          Card(
-            modifier = Modifier.clickable { onClickUserRepliedTo() },
-            shape = RoundedCornerShape(9.dp),
-            elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)
-          ) {
-            Row(
-              modifier = Modifier,
-              horizontalArrangement = Arrangement.Center,
-              verticalAlignment = Alignment.CenterVertically
-            ) {
-              Spacer(Modifier.width(2.dp))
-              if (avatarData != null)
-                Avatar(avatarData = avatarData, size = 13.dp)
-              Spacer(Modifier.width(5.dp))
-              Text(text = avatarData?.name ?: "unavailable")
-              Spacer(Modifier.width(5.dp))
-            }
-          }
-        }
-        Row() {
-          rootMessageNode.Display(
-            modifier = Modifier,
-            textStyle = null,
-            onClickedEvent = parsedNodeClickHandlerLogger
-          )
-        }
+  ReplyBox {
+    Column(
+      modifier = Modifier
+        .replyModifier()
+    ) {
+      InReplyToCard(avatarData=avatarData, onClickUserRepliedTo = onClickUserRepliedTo)
+      Row(modifier = Modifier.padding(start = 10.dp, top = 10.dp)) {
+        rootMessageNode.Display(
+          modifier = Modifier, textStyle = null, onClickedEvent = parsedNodeClickHandlerLogger
+        )
       }
     }
   }
